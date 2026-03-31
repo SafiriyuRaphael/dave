af05050ed34b8875e87d5e50d3e75ffabdf761a4
NEW_GITHUB_USERNAME <toniaroyce@gmail.com>
2026-03-31 15:08:16 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { NAVITEMS } from "./constants/NavItems";
+
+const App = () => {
+  return (
+    <div>
+      <header className="flex justify-between px-3.5 pt-3.5 items-center">
+        <h1 className="text-2xl font-bold">My App</h1>
+        <nav>
+          <ul className="flex space-x-10">
+            {NAVITEMS.map((item) => (
+              <li className="border-b border-b-white hover:border-b-black">
+                <a href={item.link}>{item.name}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <button>Click me</button>
+      </header>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/constants/NavItems.ts
@@ -0,0 +1,8 @@
+export const NAVITEMS = [
+    { name: "Home", link: "/" },
+    { name: "About", link: "/about" },
+    { name: "Services", link: "/services" },
+    { name: "Blog", link: "/blog" },
+    { name: "Subscribe", link: "/subscribe" },
+    { name: "Contact", link: "/contact" },
+];
